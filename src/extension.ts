1cedefe5b9dcc2eb5649530ed1aba28ef191a370
Nauris Pūķis <hi@pyronaur.com>
2023-01-29 16:00:18 +0200

    Jump to Recent v0.0.1

---
+++ src/extension.ts
@@ -0,0 +1,121 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+type RecentFile = {
+	path: string;
+	timestamp: number;
+};
+
+let recentFiles: RecentFile[] = [];
+
+function pushRecentDocument(document: vscode.TextDocument) {
+	if (!document) {
+		debugger;
+	}
+	// Ignore untitled files and git files
+	if (document.isUntitled || document.fileName.indexOf("git") !== -1) {
+		return;
+	}
+
+	let found = recentFiles.find(file => file.path === document.fileName);
+
+	if (found) {
+		found.timestamp = Date.now();
+		return;
+	}
+	recentFiles.push({
+		path: document.fileName,
+		timestamp: Date.now()
+	});
+}
+let loaded = false;
+
+function getAllOpenFiles() {
+	return vscode.window.tabGroups.all
+		.flatMap(({ tabs }) => tabs.map((tab: vscode.Tab) => {
+			// @ts-ignore - There's a VSCode source code bug - it can't find the input.uri. This is a workaround.
+			return tab.input.uri.path as string;
+		}));
+}
+
+async function autoloadActiveFiles() {
+	if (loaded) {
+		return;
+	}
+	loaded = true;
+	const docs = getAllOpenFiles();
+	recentFiles = [
+		...recentFiles,
+		...docs.map(path => ({
+			path: path,
+			timestamp: Date.now()
+		}))
+	];
+}
+
+function handleUserInput(path: string) {
+	if (path.trim() === '') {
+		return;
+	}
+
+	const targetDocument = vscode.workspace.openTextDocument(path);
+	targetDocument.then(function (document) {
+		vscode.window.showTextDocument(document);
+	});
+}
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let selectedIndex = -1;
+	let disposable = vscode.commands.registerCommand('jump-to-recent.quickPick', function () {
+
+		autoloadActiveFiles();
+
+		if (!recentFiles.length) {
+			vscode.window.showInformationMessage('No recent files found.');
+			return;
+		}
+
+		type RecentFileItem = vscode.QuickPickItem & { path: string };
+		const items: RecentFileItem[] = recentFiles
+			.sort((a, b) => b.timestamp - a.timestamp)
+			.map(item => {
+				const fileRelativePath = vscode.workspace.asRelativePath(item.path, false);
+				return {
+					label: fileRelativePath,
+					path: item.path,
+				};
+			})
+			.slice(1);
+
+		const quickPick = vscode.window.createQuickPick<RecentFileItem>();
+		quickPick.items = items;
+		quickPick.activeItems = [items[++selectedIndex % items.length]];
+		quickPick.show();
+		quickPick.onDidAccept(() => {
+			const selected = quickPick.activeItems[0];
+			if (selected) {
+				handleUserInput(selected.path);
+				selectedIndex = -1;
+				quickPick.hide();
+			}
+		});
+
+	});
+
+
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		if (!editor) {
+			return;
+		}
+		pushRecentDocument(editor.document);
+	}));
+	context.subscriptions.push(vscode.workspace.onDidOpenTextDocument(document => {
+		pushRecentDocument(document);
+	}));
+}
+
+export function deactivate() { }
